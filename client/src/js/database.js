f8082ef42e71e99164f9b22091f39a72a9cee453
Inigo de Robina <iderobina@MacBook-Air-2.local>
2023-11-02 15:40:16 -0600

    database.js done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,46 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => 
+{console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+//create connection to db and versin
+const textDb = await openDB("jate", 1);
+
+//Create new transaction 
+const tx = textDb.transaction("jate", "readwrite");
+
+//Opens desired object store
+const store = tx.objectStore("jate");
+
+//put method to store
+const request = store.put({ id: 1, value: content});
+
+//confirm request
+const result = await request;
+console.log("data saved to db", result)
+};
+
+//get content from db
+export const getDB = async() => {
+  console.log("det data from database");
+
+
+//create connection to db and version
+const textDb = await openDB("jate", 1);
+
+//create transaction 
+const tx = textDb.transaction("jate", "readonly");
+
+//open up desired store
+const store = tx.objectStore("jate");
+
+const request = store.get(1);
+
+//confirm the request
+const result = await request;
+console.log("request.value", result)
+return result?.value
+};
 
 initdb();
